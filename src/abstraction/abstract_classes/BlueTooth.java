a5ae3d85c7937147116c6f53b262772d6f220985
akim92270 <akim.92270@gmail.com>
2022-04-21 00:44:33 -0500

    added static and default method

--- src/abstraction/abstract_classes/BlueTooth.java
+++ src/abstraction/abstract_classes/BlueTooth.java
@@ -2,4 +2,16 @@ package abstraction.abstract_classes;
 
 public interface BlueTooth {
     void connectBlueTooth();
+
+    //Methods that has body - later versions of Java
+
+    //static method
+    static int getYear(){
+        return 2022;
+    }
+
+    //Default Method (non-static method that has body)
+    default boolean isConnected(){
+        return true;
+    }
 }
